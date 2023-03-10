fe1d13506876df25e2e99ac2dcc28f7059bebfbf
Dániel Arató <nil.the.human@gmail.com>
2023-03-10 02:41:10 +0100

    Bundles are passé, you keep plugins in pack/ now

--- upgrade-all-plugins.sh
+++ upgrade-all-plugins.sh
@@ -2,15 +2,15 @@
 
 # This script will pull the newest version of each Vim plugin from GitHub
 
-BUNDLEDIR="$HOME/.vim/bundle"
-until [[ -d "$BUNDLEDIR" ]]; do
+PACKDIR="$HOME/.vim/pack"
+until [[ -d "$PACKDIR" ]]; do
     if [[ ! -t 0 ]]; then
         # non-interactive shell
         exit 1;
     fi
-    echo "$BUNDLEDIR does not exist. Where do you keep your plugins?"
-    read -p "BUNDLEDIR = " BUNDLEDIR
-    BUNDLEDIR=`echo $BUNDLEDIR | sed s#~#\$HOME#g`
+    echo "$PACKDIR does not exist. Where do you keep your plugins?"
+    read -p "PACKDIR = " PACKDIR
+    PACKDIR=`echo $PACKDIR | sed s#~#\$HOME#g`
 done
 
 which git 1> /dev/null
@@ -22,36 +22,36 @@ if [[ $? != 0 ]]; then
     echo "You don't seem to have git installed."
     read -p "Install it now? (y/N) " USERINPUT
     if [ "$USERINPUT" != "y" ]; then
-        unset BUNDLEDIR USERINPUT
+        unset PACKDIR USERINPUT
         echo "Abort."
         exit 0;
     fi
     sudo apt-get install git --yes
 fi
 
+ALL_PLUGINS=()
 MAX_PLUGIN_LENGTH=0
-for plugin in $BUNDLEDIR/*/; do
-    if [[ $MAX_PLUGIN_LENGTH -lt ${#plugin} ]]; then
-        MAX_PLUGIN_LENGTH=${#plugin}
-    fi
+for vendor in $PACKDIR/*; do
+    for optstart in $vendor/*; do
+        for plugin in $optstart/*; do
+            ALL_PLUGINS+=($plugin)
+            if [[ $MAX_PLUGIN_LENGTH -lt ${#plugin} ]]; then
+                MAX_PLUGIN_LENGTH=${#plugin}
+            fi
+        done
+    done
 done
-((MAX_PLUGIN_LENGTH--))
 FORMAT="Upgrading %-${MAX_PLUGIN_LENGTH}s: "
 
 pushd . > /dev/null
-for plugin in $BUNDLEDIR/*/; do
-    if [ "$BASH_VERSINFO" -lt 4 ]; then
-        printf "$FORMAT" $plugin
-    else
-        printf "$FORMAT" ${plugin::-1}
-    fi
+for plugin in ${ALL_PLUGINS[@]}; do
+    printf "$FORMAT" $plugin
     cd $plugin
     git pull --no-stat
-    cd ..
 done
 popd > /dev/null
 
-unset BUNDLEDIR USERINPUT MAX_PLUGIN_LENGTH FORMAT plugin
+unset PACKDIR USERINPUT MAX_PLUGIN_LENGTH FORMAT plugin
 
 echo "Done."
 
